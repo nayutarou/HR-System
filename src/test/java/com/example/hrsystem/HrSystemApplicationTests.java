965df0d9be45f1d15414a858f51d469d2245dd3a
akira <a.oono.sys23@morijyobi.ac.jp>
2025-11-07 11:01:13 +0900

    1. テスト用の設定ファイルを作成: src/test/resources ディレクトリに application-test.properties       というファイルを作成し、そこでデータベース関連の自動設定をまとめて無効化します。    2. テストクラスでプロファイルを有効化: HrSystemApplicationTests で @ActiveProfiles("test") アノテーションを使い、テスト実行時に       application-test.properties を読み込むように指示します。

--- src/test/java/com/example/hrsystem/HrSystemApplicationTests.java
+++ src/test/java/com/example/hrsystem/HrSystemApplicationTests.java
@@ -1,20 +1,11 @@
 package com.example.hrsystem;
 
 import org.junit.jupiter.api.Test;
-import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class,
-        MybatisAutoConfiguration.class
-})
+@ActiveProfiles("test")
 class HrSystemApplicationTests {
 
     @Test
