38e1803410ae0ec1881828205aa431b820f2c7c7
akira <a.oono.sys23@morijyobi.ac.jp>
2025-11-07 10:53:32 +0900

    HrSystemApplicationTests.java ファイルを修正し、@SpringBootTest   アノテーションにデータベース自動設定を無効化するプロパティを追加します。

--- src/test/java/com/example/hrsystem/HrSystemApplicationTests.java
+++ src/test/java/com/example/hrsystem/HrSystemApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.hrsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class HrSystemApplicationTests {
 
     @Test
