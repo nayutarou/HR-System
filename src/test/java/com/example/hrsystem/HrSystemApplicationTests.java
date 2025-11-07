bf3df4e99c96e447411342827daedcc5c8ad166f
akira <a.oono.sys23@morijyobi.ac.jp>
2025-11-07 10:57:55 +0900

    DataSource に加え、TransactionManager、JPA、そして Mybatis の自動設定を除外対象に追加しました。これで、テスト実行時にデータベース関   連のコンポーネントが一切ロードされなくなり、コンテキストのロードに成功するはずです。

--- src/test/java/com/example/hrsystem/HrSystemApplicationTests.java
+++ src/test/java/com/example/hrsystem/HrSystemApplicationTests.java
@@ -1,10 +1,19 @@
 package com.example.hrsystem;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        MybatisAutoConfiguration.class
 })
 class HrSystemApplicationTests {
 
